d242aabedb20bc0d16032a12912a6bc985b9a63b
JORGE ALBERTO TORRES BAUTISTA <jorge.torres.bautista@ibm.com>
2020-08-21 21:45:16 -0500

    Ejercicio #Uso de variables , operacion matematica basica y solicitud de información

---
+++ ejercicio01.py
@@ -0,0 +1,10 @@
+#Ejemplo de uso de string 
+#Jorge Torres
+#21 Agosto 2020
+
+NumEntero = int(input("Captura un numero entero"))
+NumDecimal = float(input("Captura un numero con punto decimal"))
+Nombre = (input("Captura tu Nombre"))
+Division = NumEntero/NumDecimal
+
+print("Hola",Nombre,"el resultado es:", Division)
